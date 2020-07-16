cb21e41c3d9b30b2030a8a890d34b7c2f9126e4c
dlwangyilin <dlwangyilin@gmail.com>
2020-07-16 15:04:02 -0700

    Finish auth

--- squeaker-client/src/Containers/Main.js
+++ squeaker-client/src/Containers/Main.js
@@ -2,12 +2,32 @@ import React from "react";
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import Homepage from "../Components/Homepage";
+import AuthForm from "../Components/AuthForm";
+import {authUser} from "../store/actions/auth";
 
 const Main = props => {
+    const {authUser} = props;
     return (
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props => <Homepage {...props}/>} />
+                <Route exact path="/signin" render={props => {
+                    return (
+                        <AuthForm {...props}
+                            onAuth={authUser}
+                            buttonText="Log in"
+                            heading="Welcome Back."/>
+                    );
+                }} />
+                <Route exact path="/signup" render={props => {
+                    return (
+                        <AuthForm {...props}
+                              onAuth={authUser}
+                              buttonText="Sign me up!"
+                              heading="Join Squeaker today."
+                              signUp/>
+                    );
+                }} />
             </Switch>
         </div>
     );
@@ -19,4 +39,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, {authUser})(Main));
